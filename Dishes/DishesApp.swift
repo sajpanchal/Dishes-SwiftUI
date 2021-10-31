5e9b44b9e2c94cb53841c2ab45ecafbeb2dbf580
saj panchal <sajpanchal2020@outlook.com>
2021-10-31 11:11:43 -0400

    project initiated.

---
+++ Dishes/DishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishesApp.swift
+//  Dishes
+//
+//  Created by saj panchal on 2021-10-31.
+//
+
+import SwiftUI
+
+@main
+struct DishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
